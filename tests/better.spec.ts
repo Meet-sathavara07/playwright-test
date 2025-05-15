9f27395cf4f5d093b74c4e85037432e7e928c969
meet sathavare <meetsathavara10@gmail.com>
2025-05-15 15:34:41 +0530

    fully login-send-delete all working fine

--- tests/better.spec.ts
+++ tests/better.spec.ts
@@ -46,12 +46,37 @@ test("user login and send and view a kudo", async ({ page }) => {
   await page.getByRole("button", { name: "Send" }).isVisible();
 
   await page.getByRole("textbox", { name: "Email Id" }).click();
-  await page
-    .getByRole("textbox", { name: "Email Id" })
-    .fill("raj.mansuri@quantuminfoway.com");
+  await page.getByRole("textbox", { name: "Email Id" }).fill("raj.mansuri@quantuminfoway.com");
   await page.getByRole("button", { name: "Send" }).click();
 
   await page.getByRole("link", { name: "Go To Feed" }).click();
-  await expect(page).toHaveURL("https://www.cheerchampion.com/feeds-stop");
+  await expect(page).toHaveURL("https://www.cheerchampion.com/feeds");
 
+  await page.locator('.bg-white > div > div').first().isVisible();
+  await page.locator('div').filter({ hasText: /^Youto test_Raj$/ }).nth(1).isVisible();
+  await page.getByRole('link', { name: 'You' }).first().isVisible();
+  await page.locator('span').filter({ hasText: 'test_Raj' }).getByRole('link').isVisible();
+
+  await page.getByRole('button', { name: 'Received Received' }).click();
+
+  await page.locator('div').filter({ hasText: /^test_Rajto You$/ }).first().isVisible();
+  await page.getByRole('link', { name: 'test_Raj' }).isVisible();
+  await page.getByRole('link', { name: 'You' }).nth(1).isVisible();
+
+  await page.getByRole('button', { name: 'Given Given' }).click();
+
+  await page.locator('div').filter({ hasText: /^Youto test_Raj$/ }).nth(1).isVisible();
+  await page.getByRole('link', { name: 'You' }).first().isVisible();
+  await page.locator('span').filter({ hasText: 'test_Raj' }).getByRole('link').isVisible();
+
+  await page.getByRole('button', { name: 'All' }).click();
+
+  await page.locator('div:nth-child(2) > .rounded-full').first().click();
+  await page.getByRole('button', { name: /Delete/i }).first().click();
+  await page.getByRole('button', { name: 'Delete' }).click();
+
+
+  await page.getByRole('button', { name: 'profile' }).click();
+  await page.getByRole('button', { name: 'Sign out' }).click();
 });
+
